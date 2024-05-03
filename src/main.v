1c7b33f6ba00884ec7f9591f3358492cc119b2ac
Helder de Sousa <helderhenri@gmail.com>
2024-05-03 09:30:27 -0300

    tests: with real file

--- src/main.v
+++ src/main.v
@@ -1,7 +1,15 @@
 module main
 
-import lexer
+import os
+import parser
 
 fn main() {
+	path := os.args[1]
+	source := os.read_file(path) or { panic(err)}
+	response := parser.parse_stmt(source) or {
+		println(err.msg())
+		exit(1)
+		}
+	println(response)
 	exit(0)
 }
