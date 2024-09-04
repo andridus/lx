fd45645676af7deb7c7e16b42e2a3ff1d2f6a1af
Helder de Sousa <helderhenri@gmail.com>
2024-09-04 08:22:33 -0300

    chore: improve tests

--- src/main.v
+++ src/main.v
@@ -3,14 +3,45 @@ module main
 import os
 import parser
 import color
+import testing
+import flag
+const version = '0.1.0'
+@[xdoc: 'Lx compiler']
+@[footer: '']
+@[version: version]
+@[name: 'lx']
+struct Config {
+    show_version bool @[short: v; xdoc: 'Show version and exit']
+		show_help bool @[short: h; xdoc: 'Show help']
+		test_suite bool @[long: suite; xdoc: 'Run tests suite']
+		file string
+}
 
 fn main() {
-	if os.args.len == 1 {
-		println('file not valid')
-		exit(1)
+	config, no_matches := flag.to_struct[Config](os.args, skip: 1)!
+
+	if config.show_help {
+		documentation := flag.to_doc[Config]()!
+		println(documentation)
+		exit(0)
+	}
+	if config.show_version {
+		println('Lx compiler ${version}')
+		exit(0)
+	}
+	if config.test_suite {
+		testing.run_suite()
+	}
+
+	if no_matches.len != 1 {
+			println('Invalid command, show help with `-h`')
+			exit(0)
 	}
 	path := os.args[1]
-	source := os.read_bytes(path) or { panic(err) }
+	source := os.read_bytes(path) or {
+		println('File \'${path}\' not found')
+		exit(1)
+	}
 	nodes := parser.parse_stmts(source) or { exit(1) }
 	for node in nodes {
 		println(node.elixir())
---
+++ src/testing/testing.v
@@ -0,0 +1,24 @@
+module testing
+
+import color
+import os
+import parser
+
+const root_path = 'test'
+pub fn run_suite() bool {
+
+	paths := os.ls('${@VMODROOT}/${root_path}') or {
+		println(color.fg(.dark_red, .default, 'ERROR: ') + err.msg())
+		exit(1)
+	}
+	println('Test Suite: Run compile over ${paths.len} files \n----')
+	for path in paths {
+		println(color.fg(.white, .default, 'Compiling ${root_path}/${path}'))
+		source := os.read_bytes('${root_path}/${path}') or { return false }
+		_ := parser.parse_stmts(source) or { exit(1) }
+		println(color.fg(.dark_green, .default, '->> ${root_path}/${path} compiled with success!'))
+
+	}
+	exit(1);
+	return false
+}
